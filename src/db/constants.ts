3132dd9a0cd59c9bedb294de4bf95427016fa8f6
Cory Perkins <link2cory@gmail.com>
2020-10-11 18:12:35 -0700

    add song db model

--- src/db/constants.ts
+++ src/db/constants.ts
@@ -1,2 +1,3 @@
 export const BAND_KEY_PREFIX = "BAND-";
 export const USER_KEY_PREFIX = "USER-";
+export const SONG_KEY_PREFIX = "SONG-";
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
 export * from "./Band";
 export * from "./User";
 export * from "./BandMembership";
+export * from "./Song";
